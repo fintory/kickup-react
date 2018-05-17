30b498604e6cb4409de2f08515f57042b00ad6a5
Nick Schmidt <nick@kreativgebiet.com>
2018-05-17 12:56:46 +0200

    continue implementation of server-side rendeirng

--- server/middlewares/frontendMiddleware.js
+++ server/middlewares/frontendMiddleware.js
@@ -1,83 +1,113 @@
+// @flow
 /* eslint-disable global-require */
-const React = require('react');
-const express = require('express');
-const path = require('path');
-const compression = require('compression');
+import React from 'react'
+import express from 'express'
+import path from 'path'
+
+/* eslint-disable import/no-extraneous-dependencies */
+import MemoryFS from 'memory-fs'
+import webpack from 'webpack'
+import compression from 'compression'
+import requireFromString from 'require-from-string'
+/* eslint-enable import/no-extraneous-dependencies */
 
 // Dev middleware
-const addDevMiddlewares = (app, webpackConfig) => {
-  const webpack = require('webpack');
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
-  const compiler = webpack(webpackConfig);
+const addDevMiddlewares = async (app, webpackConfig): Promise<*> => {
+  /* eslint-disable import/no-extraneous-dependencies */
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware')
+  /* eslint-enable import/no-extraneous-dependencies */
+
+  const compiler = webpack(webpackConfig)
   const middleware = webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: webpackConfig.output.publicPath,
     silent: true,
     stats: 'errors-only',
-  });
+  })
 
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
+  app.use(middleware)
+  app.use(webpackHotMiddleware(compiler))
 
   // Since webpackDevMiddleware uses memory-fs internally to store build
   // artifacts, we use it instead
-  const fs = middleware.fileSystem;
+  const fs = middleware.fileSystem
 
   app.get('*', (req, res) => {
     fs.readFile(path.join(compiler.outputPath, 'index.html'), (err, file) => {
       if (err) {
-        res.sendStatus(404);
+        res.sendStatus(404)
       } else {
-        res.send(file.toString());
+        res.send(file.toString())
       }
-    });
-  });
-};
+    })
+  })
+}
 
 // Production middlewares
-const addProdMiddlewares = (app, options) => {
-  const ReactDOMServer = require('react-dom/server');
-  const App = require('../../client');
+const addProdMiddlewares = async (app, config): Promise<*> => {
+  try {
+    const ReactDOMServer = require('react-dom/server')
+    // const App = require('../../client/pages/Root')
 
-  const publicPath = options.publicPath || '/';
-  const outputPath = options.outputPath || path.resolve(process.cwd(), 'build');
+    const publicPath = '/'
+    const outputPath = path.resolve(process.cwd(), 'build')
 
-  // compression middleware compresses your server responses which makes them
-  // smaller (applies also to assets). You can read more about that technique
-  // and other good practices on official Express.js docs http://mxs.is/googmy
-  app.use(compression());
-  app.use(publicPath, express.static(outputPath));
+    const fs = new MemoryFS()
+    const serverCompiler = webpack(config)
+    serverCompiler.outputFileSystem = fs
 
-  function handleRender(req, res) {
-    const html = ReactDOMServer.renderToString(<App />);
+    // compression middleware compresses your server responses which makes them
+    // smaller (applies also to assets). You can read more about that technique
+    // and other good practices on official Express.js docs http://mxs.is/googmy
+    app.use(compression())
+    app.use(publicPath, express.static(outputPath))
 
-    res.readFile(path.resolve(outputPath, 'index.html'), 'utf8', function (err, data) {
-      if (err) throw err;
+    serverCompiler.run((err, stats) => {
+      const contents = fs.readFileSync(
+        path.resolve(config.output.path, config.output.filename),
+        'utf8'
+      )
+      const App = requireFromString(contents)
 
-      // Inserts the rendered React HTML into our main div
-      const document = data.replace(/<div id="root"><\/div>/, `<div id="root">${html}</div>`);
+      function handleRender(req, res) {
+        const html = ReactDOMServer.renderToString(
+          // <MemoryRouter>
+          <App />
+          // </MemoryRouter>
+        )
 
-      // Sends the response back to the client
-      res.send(document);
-    });
+        res.readFile(path.resolve(outputPath, 'index.html'), 'utf8', (e, data) => {
+          if (e) throw e
+
+          // Inserts the rendered React HTML into our main div
+          const document = data.replace(/<div id="root"><\/div>/, `<div id="root">${html}</div>`)
+
+          // Sends the response back to the client
+          res.send(document)
+        })
+      }
+
+      app.get('*', handleRender)
+    })
+  } catch (err) {
+    console.error(err)
   }
-
-  app.get('*', handleRender);
-};
+}
 
 /**
  * Front-end middleware
  */
-module.exports = (app, options) => {
-  const isProd = process.env.NODE_ENV === 'production';
+module.exports = async (app, options): Promise<{}> => {
+  const isProd = process.env.NODE_ENV === 'production'
 
   if (isProd) {
-    addProdMiddlewares(app, options);
+    const webpackConfig = require('../../config/webpack.config.prod.babel')
+    await addProdMiddlewares(app, webpackConfig)
   } else {
-    const webpackConfig = require('../../config/webpack.config.dev.babel');
-    addDevMiddlewares(app, webpackConfig);
+    const webpackConfig = require('../../config/webpack.config.dev.babel')
+    await addDevMiddlewares(app, webpackConfig)
   }
 
-  return app;
-};
+  return app
+}
