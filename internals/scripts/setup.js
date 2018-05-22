4a1fc399f06b7f71e31fea074d1a727d24d177ef
Nick Schmidt <nick@kreativgebiet.com>
2018-05-22 16:48:30 +0200

    add first version of setup script

---
+++ internals/scripts/setup.js
@@ -0,0 +1,50 @@
+// @flow
+/* eslint-disable import/no-extraneous-dependencies */
+import rimraf from 'rimraf'
+import ora from 'ora'
+import { exec } from 'child_process'
+/* eslint-enable import/no-extraneous-dependencies */
+
+async function wrapPromise(name: string, callback: Function): Promise<*> {
+  return new Promise(resolve => {
+    const logger = ora(name).start()
+
+    callback(() => {
+      logger.succeed()
+      resolve()
+    })
+  })
+}
+
+async function removeGitFolder(): Promise<*> {
+  return wrapPromise('Remove old `.git` folder', resolve => rimraf('./static', null, resolve))
+}
+
+async function installDependencies(): Promise<*> {
+  return wrapPromise(resolve =>
+    exec('yarn --version', (err, stdout) => {
+      if (parseFloat(stdout) < 0.15 || err || process.env.USE_YARN === 'false') {
+        exec('npm install', resolve)
+      } else {
+        exec('yarn install', resolve)
+      }
+    })
+  )
+}
+
+async function initializeGitRepository(): Promise<*> {
+  return wrapPromise(resolve =>
+    exec('git init && git add . && git commit -m "Initial commit"', resolve)
+  )
+}
+
+async function removeThisFile(): Promise<*> {
+  return wrapPromise(resolve => rimraf(`${__dirname}/setup.js`, resolve))
+}
+
+;(async function setup(): Promise<*> {
+  await removeGitFolder()
+  await initializeGitRepository()
+  await installDependencies()
+  await removeThisFile()
+})()
