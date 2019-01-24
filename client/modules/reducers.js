1919d27ee7d55a98751fff87b287bc34c0408396
Nick Schmidt <nick@kreativgebiet.com>
2019-01-24 12:57:20 +0100

    chore: update connected-react-router & react-redux

--- client/modules/reducers.js
+++ client/modules/reducers.js
@@ -1,10 +1,13 @@
 // @flow
 import { combineReducers } from 'redux'
+import { connectRouter } from 'connected-react-router'
 import type { CombinedReducer } from 'redux'
 
+import history from './history'
 import { reducer as tasksReducer } from './tasks'
 import type { Action } from './types'
 
 export const rootReducer: CombinedReducer<any, Action> = combineReducers({
+  router: connectRouter(history),
   tasks: tasksReducer,
 })
